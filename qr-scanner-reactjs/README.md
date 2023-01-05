4fa328c1b0e1bb61e36c489593044987233e25e2
Juhaina Mufassir <110373605+juhaina1996@users.noreply.github.com>
2023-01-05 21:19:56 +0530

    my WR code

---
+++ qr-scanner-reactjs/README.md
@@ -0,0 +1,4 @@
+# QR Scanner Reactjs
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/codingstella/pen/rNKRwGz](https://codepen.io/codingstella/pen/rNKRwGz).
+
---
+++ qr-scanner-reactjs/src/script.js
@@ -0,0 +1,45 @@
+const QrCode = ({ url, title, text }) => {
+  const qrcode = React.useRef(null);
+
+  React.useEffect(() => {
+    const el = qrcode.current.querySelector(".qr-code-code");
+
+    const code = new QRCode(el, {
+      text: url,
+      width: 160,
+      height: 160,
+      colorDark: "#FFFFFF",
+      colorLight: "#3685fe",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+
+    el.title = ""; // to remove built-in tooltip in qr-code library
+
+    return () => code.clear();
+  }, []);
+
+  return /*#__PURE__*/ React.createElement(
+    "div",
+    { className: "qr-code", ref: qrcode } /*#__PURE__*/,
+    React.createElement("div", { className: "qr-code-code" }) /*#__PURE__*/,
+    React.createElement(
+      "div",
+      { className: "qr-code-content" } /*#__PURE__*/,
+      React.createElement("h3", null, title) /*#__PURE__*/,
+      React.createElement("p", null, text)
+    )
+  );
+};
+
+const App = () => {
+  return /*#__PURE__*/ React.createElement(QrCode, {
+    url: "https://github.com/juhaina1996",
+    title: "Juhaina",
+    text: "Scan the QR code to visit my Profile",
+  });
+};
+
+ReactDOM.render(
+  /*#__PURE__*/ React.createElement(App, null),
+  document.getElementById("app")
+);
